e285f2ba59b9c9380e123c65af20e10e7cb4e9e5
afina5710-max <afina5710@gmail.com>
2026-06-02 16:51:21 +0700

    perubahan middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,12 @@
-export { default } from "next-auth/middleware";
-
 export const config = {
-  matcher: ["/dashboard/:path*"],
-};
+  matcher: [
+    /*
+     * Cocokkan semua rute request KECUALI:
+     * 1. /api (rute API internal & NextAuth)
+     * 2. _next/static (file statis)
+     * 3. _next/image (optimasi gambar)
+     * 4. favicon.ico, sitemap.xml, robots.txt (file metadata)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+}
